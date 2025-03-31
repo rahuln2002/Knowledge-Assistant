39cfe266a4a32f4fdcf263391fcfb8d7e146f55f
Rahul Nihalani <itsnrahul@gmail.com>
2025-04-01 00:25:47 +0530

    Deployment Changes

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def home():
     return render_template("index.html", task=None, text=None, user_text=None)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=8080)
+    app.run(host='0.0.0.0', port=8080)
